b5ab46319d047a09722dcda54a374a9266337863
Sofia Ysabel Gange <sofiaysabelgange@gmail.com>
2023-06-26 11:03:08 -0700

    Initial Commit

---
+++ kode with klossy github/kode_with_klossy_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  kode_with_klossy_githubApp.swift
+//  kode with klossy github
+//
+//  Created by Sofia Gange on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct kode_with_klossy_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
